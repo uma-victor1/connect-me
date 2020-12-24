9a56454688f4d14bd834b8280d255cf4d58e7210
umavictor <umavictor11@gmail.com>
2020-12-24 17:55:46 +0100

    added vuejs-datepicker library and fleshed out EventCreate page

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,53 @@
 <template>
-  <h1 class="text-green-400">Create Event</h1>
+  <div>
+    <h1>Create an Event</h1>
+    <form @submit.prevent="createEvent">
+      <label>Select a category</label>
+      <select>
+        <option></option>
+      </select>
+
+      <h3>Name & describe your event</h3>
+      <div class="field">
+        <label>Title</label>
+        <br />
+        <input type="text" placeholder="Add an event title" />
+      </div>
+
+      <div class="field">
+        <label>Description</label>
+        <input type="text" placeholder="Add a description" />
+      </div>
+
+      <h3>Where is your event?</h3>
+      <div class="field">
+        <label>Location</label>
+        <input type="text" placeholder="Add a location" />
+      </div>
+
+      <h3>When is your event?</h3>
+
+      <div class="field">
+        <label>Date</label>
+        <datepicker placeholder="Select a date" />
+      </div>
+
+      <div class="field">
+        <label>Select a time</label>
+        <select>
+          <option v-for="time in times" :key="time">{{ time }}</option>
+        </select>
+      </div>
+
+      <input type="submit" class="button -fill-gradient" value="Submit" />
+    </form>
+  </div>
 </template>
+<script>
+import Datepicker from 'vuejs-datepicker'
+export default {
+  components: {
+    Datepicker
+  }
+}
+</script>
