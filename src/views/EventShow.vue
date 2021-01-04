f0d33c7d9c53d4b89bb9eff61d47518a18b0a0e9
umavictor <umavictor11@gmail.com>
2021-01-04 07:30:10 +0100

    remove all vuex code from event-show component

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -32,9 +32,12 @@
   </div>
 </template>
 <script>
-import { mapState } from 'vuex'
 export default {
-  props: ['id'],
-  computed: mapState({ event: state => state.event.event })
+  props: {
+    event: {
+      type: Object,
+      required: true
+    }
+  }
 }
 </script>
