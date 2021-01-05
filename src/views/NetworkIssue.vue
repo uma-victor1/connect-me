70a09eb56da74667d5f6a4077def70fbe88c5813
umavictor <umavictor11@gmail.com>
2021-01-05 07:12:58 +0100

    Created a network error page

---
+++ src/views/NetworkIssue.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col h-full items-center justify-center">
+    <h1 class="text-3xl font-medium">uh-ohh!!</h1>
+    <p class="text-2xl">
+      Looks like you're experiencing some network issues. Go back to home page
+    </p>
+    <router-link :to="{ name: 'event-list' }">Back to home page</router-link>
+  </div>
+</template>
