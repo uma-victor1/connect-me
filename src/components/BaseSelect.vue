294fff316ecd5a3ac2c2e46442be6a68c2128905
umavictor <umavictor11@gmail.com>
2021-01-09 07:55:36 +0100

    added category option in eventcreate

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -3,13 +3,13 @@
     <label v-if="label" class="block text-sm font-medium text-gray-700">{{
       label
     }}</label>
-    <input
+    <select
       type="text"
       :value="value"
       v-bind="$attrs"
       class="mt-1 px-4 py-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
       @input="updateValue"
-    />
+    ></select>
   </div>
 </template>
 
