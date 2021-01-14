1747d69a9e461fbc41936756c3887a762edea19f
umavictor <umavictor11@gmail.com>
2021-01-14 09:48:05 +0100

    added padding to card

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -3,9 +3,9 @@
     class="eventlink"
     :to="{ name: 'event-show', params: { id: event.id } }"
   >
-    <div class="mb-4 shadow-md px-7 py-10 hover:shadow-xl">
-      <span>@{{ event.time }} on {{ event.date }}</span>
-      <h4>{{ event.title }}</h4>
+    <div class="mb-4 shadow-md px-7 py-7 hover:shadow-xl">
+      <span class="mb-4">@{{ event.time }} on {{ event.date }}</span>
+      <h4 class="mb-2 mt-2">{{ event.title }}</h4>
       <BaseIcon name="users"
         >{{ event.attendees ? event.attendees.length : 0 }} attending</BaseIcon
       >
